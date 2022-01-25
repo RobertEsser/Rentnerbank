4cc118514344f35de4e32d18ab9d2f12a2a10d5a
Michael Russe <michael.russe@telekom.de>
2022-01-25 13:07:37 +0100

    Update

--- src/main/java/de/telekom/sea7/View/BookingsViewImpl.java
+++ src/main/java/de/telekom/sea7/View/BookingsViewImpl.java
@@ -1,26 +1,31 @@
 package de.telekom.sea7.View;
 
 import java.time.LocalDateTime;
+import java.lang.Iterable;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import de.telekom.sea7.Model.BookingImpl;
-import de.telekom.sea7.Model.BookingsImpl;
+import de.telekom.sea7.Booking;
+import de.telekom.sea7.BookingView;
+import de.telekom.sea7.Bookings;
+import de.telekom.sea7.BookingsView;
 
-public class BookingsViewImpl {
+public class BookingsViewImpl implements BookingsView {
 
-	private BookingsImpl bookingsimpl;
+	private Bookings bookingsimpl;
 
-	public BookingsViewImpl(BookingsImpl bookingsimpl) {
+	public BookingsViewImpl(Bookings bookingsimpl) {
 		this.bookingsimpl = bookingsimpl;
 	}
 
+	@Override
 	public void menu() {
 		String input = "";
 		Scanner scanner = new Scanner(System.in);
 		while (!input.equals("exit")) {
-			System.out
-					.println("Enter something (e.g add - create new transaction, showAll - lists all transactions): ");
+			System.out.println(
+					"Enter something (e.g add - create new transaction,showAll - lists all transactions, showOne - List one transaction): ");
 			input = scanner.next();
 
 			switch (input) {
@@ -56,15 +61,18 @@ public class BookingsViewImpl {
 	 * TransactionView(this, (Transaction) allTrans.get(position));
 	 * transactionView.menu(); }
 	 */
-		
-	
-	
+
 	private void add() {
 		LocalDateTime datum = LocalDateTime.now();
 		Scanner scannerAdd = new Scanner(System.in);
-
 		System.out.println("Wie viel soll überwiesen werden: ");
+
+		while (!scannerAdd.hasNextFloat()) {
+			System.out.println("Es wurde kein gültiger Betrag eingegeben. Bitte um neue Eingabe: ");
+			scannerAdd.next();
+		}
 		float betrag = scannerAdd.nextFloat();
+
 		scannerAdd.nextLine();
 		System.out.println("Empfänger: ");
 		String empfaenger = scannerAdd.nextLine();
@@ -82,24 +90,22 @@ public class BookingsViewImpl {
 	private void showAll() {
 
 		for (Object o : bookingsimpl) {
-			BookingImpl tempTrans = (BookingImpl) o;
+			Booking tempTrans = (Booking) o;
 			System.out.println(bookingsimpl.getIndex(tempTrans) + "-" + tempTrans.getEmpfaenger() + " - "
 					+ tempTrans.getVerwendungszweck() + " - " + String.format("%.2f", tempTrans.getBetrag()) + "€");
 		}
-	
-	}
-private void showOne() {
-	 Scanner scannershowOne = new Scanner(System.in);
-	 System.out.println("Wähle den gewünschten Datensatz aus: "); 
-	 int index = scannershowOne.nextInt(); 
-	 scannershowOne.nextLine();
-	BookingImpl temp = bookingsimpl.getBooking(index);
-	
-	BookingViewImpl bookingviewimpl = new BookingViewImpl(temp);
-	bookingviewimpl.show();
-	
-	
-	
-}
-}
 
+	}
+
+	private void showOne() {
+		Scanner scannershowOne = new Scanner(System.in);
+		System.out.println("Wähle den gewünschten Datensatz aus: ");
+		int index = scannershowOne.nextInt();
+		scannershowOne.nextLine();
+		Booking temp = bookingsimpl.getBooking(index);
+
+		BookingView bookingviewimpl = new BookingViewImpl(temp);
+		bookingviewimpl.show();
+
+	}
+}
