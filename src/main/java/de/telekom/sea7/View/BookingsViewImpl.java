8f9af1365d4ec9afed6fa84fa49193b734a2e330
Robert Esser <robert-esser_student2021@wilder-school>
2022-01-26 07:45:53 +0100

    Update 4

--- src/main/java/de/telekom/sea7/View/BookingsViewImpl.java
+++ src/main/java/de/telekom/sea7/View/BookingsViewImpl.java
@@ -1,6 +1,9 @@
 package de.telekom.sea7.View;
 
 import java.time.LocalDateTime;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.lang.Iterable;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
@@ -25,7 +28,7 @@ public class BookingsViewImpl implements BookingsView {
 		Scanner scanner = new Scanner(System.in);
 		while (!input.equals("exit")) {
 			System.out.println(
-					"Enter something (e.g add - create new transaction,showAll - lists all transactions, showOne - List one transaction): ");
+					"Enter something (e.g add - create new transaction,showAll - lists all transactions, showOne - List one transaction, Download): ");
 			input = scanner.next();
 
 			switch (input) {
@@ -37,6 +40,9 @@ public class BookingsViewImpl implements BookingsView {
 				break;
 			case "showAll":
 				showAll();
+				break;
+			case "Download":
+				download();
 				break;
 			case "exit":
 				scanner.close();
@@ -51,6 +57,55 @@ public class BookingsViewImpl implements BookingsView {
 
 			}
 		}
+	}
+
+	private void download() {
+		try (PrintWriter writer = new PrintWriter(new File("test.csv"))) {
+
+			StringBuilder sb = new StringBuilder();
+
+			sb.append("Betrag");
+			sb.append(',');
+			sb.append("Empfaenger");
+			sb.append(',');
+			sb.append("IBAN");
+			sb.append(',');
+			sb.append("BIC");
+			sb.append(',');
+			sb.append("Verwendungszeck");
+			sb.append(',');
+			sb.append("Datum");
+			sb.append('\n');
+// 			writer.write(sb.toString()); - alte Variante
+
+			for (Object o : bookingsimpl) {
+				Booking tempTrans = (Booking) o;
+
+				sb.append(tempTrans.getBetrag());
+				sb.append(',');
+				sb.append(tempTrans.getEmpfaenger());
+				sb.append(',');
+				sb.append(tempTrans.getIban());
+				sb.append(',');
+				sb.append(tempTrans.getBic());
+				sb.append(',');
+				sb.append(tempTrans.getVerwendungszweck());
+				sb.append(',');
+				sb.append(tempTrans.getDatum());
+				sb.append('\n');
+
+//				writer.write(sb.toString()); - alte Variante
+				
+				System.out.println("done!");
+
+			}
+			writer.write(sb.toString());
+			
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+
+				
 	}
 
 	/*
@@ -84,7 +139,7 @@ public class BookingsViewImpl implements BookingsView {
 		String verwendungszweck = scannerAdd.nextLine();
 
 		bookingsimpl.add(betrag, empfaenger, iban, bic, verwendungszweck, datum);
-		// scannerAdd.close();
+		// scannerAdd.("1");close();
 	}
 
 	private void showAll() {
